3e28c709fbb0df9aaa329df8f453e2124839c274
reyesvl <reyesvictorlaurence@gmail.com>
2020-07-14 09:28:00 +0800

    agagagaga

--- Testing.R
+++ Testing.R
@@ -5,6 +5,8 @@ print("hello world how are you today")
 
 print("trial pull hehehe)
 
-was there a lifetime waiting for us in a world where i was yours 
+was there a lifetime waiting for us in a world where i was yours victor laurence reyes
 was it the wrong time what if we tried giving in a little more
 paper planes and porcelains oh you were a good dream
+
+
